2f79036cba19c184917504d763d257aad9794714
Katanac <tatisred@hotmail.com>
2022-06-05 18:58:12 -0500

    solucion primer punto y segundo punto

--- src/main/kotlin/solucion_taller.kt
+++ src/main/kotlin/solucion_taller.kt
@@ -1,17 +1,63 @@
-fun main(){
+fun main() {
     println(" Hola mundo")
 }
 
 /*
 Actividad 1 traduccion de ecuaciones
 1.
-var s= Math.sqrt(Math.pow(x-m,2.0)/(n-1))
+var S= Math.sqrt(Math.pow(x-m,2.0)/(n-1))
 
 2.
-var p= -(Math.pow(y,3.0)-1)/((y+1)-Math.sqrt(y+1))
+var P= -(Math.pow(y,3.0)-1)/((y+1)-Math.sqrt(y+1))
+3.
+var Z = (x (Math.pow((Math.pow(x, 2.0) + 1), 3.0))) / (Math.sqrt(2 x) + 1)
+
+4.
+var T = 1- ((Math.sqrt(x-2, 1.0/n))/(Math.pow(x,3.0)))
+
+5.
+var s = ((x != y) && (x <= y))
+
+6.
+var s =((a>=b(c))||(b<=c))
 
 Actividad 2
+
+a. 30+8*3/6-4%6
+  R=
+
+    30+24/6-4%6
+    30+4-4%6
+    30+4-4
+    34-4
+    30
+
+b. 30.0 / 20.0 - 2.5 * 3.0
+   R=
+
+    30.0 / 20.0 - 2.5 * 3.0
+    30.0 / 20.0 - 7.5
+    30.0 / 20.0 - 7.5
+    1.5-7.5
+    -6.0
+
+c. 8/4/2*6
+
+   R=
+
+    8/4/2*6
+    8/4/12
+    2/12
+    0.17
+
 * */
+
+
+
+
+
+
+
 
 
 
