4adb3fcae69828e591441ebf754599a1564f59e8
Katanac <tatisred@hotmail.com>
2022-06-05 18:04:13 -0500

    primmer commit

---
+++ src/main/kotlin/solucion_taller.kt
@@ -0,0 +1,17 @@
+fun main(){
+    println(" Hola mundo")
+}
+
+/*
+Actividad 1 traduccion de ecuaciones
+1.
+var s= Math.sqrt(Math.pow(x-m,2.0)/(n-1))
+
+2.
+var p= -(Math.pow(y,3.0)-1)/((y+1)-Math.sqrt(y+1))
+
+Actividad 2
+* */
+
+
+
