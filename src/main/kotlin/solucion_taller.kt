69e7a13e679a7dd1ca9dd0a30f5889c2633853c3
Katanac <tatisred@hotmail.com>
2022-06-05 21:55:21 -0500

    cambios actividad 3 tatiana

--- src/main/kotlin/solucion_taller.kt
+++ src/main/kotlin/solucion_taller.kt
@@ -50,14 +50,312 @@ c. 8/4/2*6
     2/12
     0.17
 
+
+   Actividad 3
+
 * */
 
 
+/*
+a. Realice una función que halle el área de un triángulo.
+Entradas:
+
+ base-> double->b
+ altura-> double->a
+
+Salidas:
+
+Area -> double->ar
+* */
+
+fun calculoAreaTrianglo(b: Double, a: Double): Double {
+    val ar = (b * a) / 2
+    return ar
+}
 
 
+/*
+b. Si alquilar una bicicleta tiene cierto valor y puede transportar dos personas, determine cuántas bicicletas
+necesito y cuánto dinero voy a gastar en alquilarlas, para transportar a todos los estudiantes de la
+universidad.
+
+Entradas:
+
+valor alquiler -> double -> va
+cantidad estudiantes -> double -> ce
+
+Salidas:
+
+biciletas necesarias -> double -> bn
+total dinero del alquiler -> double -> da
+
+* */
+
+fun alquilerBiciletas(ce: Double, va: Double): Pair<Double, Double> {
+    val bn = ce / 2
+    val da = bn * va
+    return Pair(bn, da)
+
+}
 
 
+/*
+d. Se desea determinar cuántos dólares se puede adquirir con cierta cantidad de pesos colombianos
+
+Entradas:
+peso colombiano -> double -> pc
+dolar actual -> double -> da
+
+Salidas:
+
+dolares adquiridos -> double -> d
+* */
+
+fun conversion(pc: Double, da: Double): Double {
+    val d = pc / da
+    return d
+}
+
+/*
+f. Un estacionamiento requiere determinar el cobro que debe aplicar a las personas que lo utilizan. Considere
+que el cobro es con base en las horas que lo disponen y que las fracciones de hora se toman como completas
+yrealice una función que permita determinar el cobro.
 
 
+Entradas:
+cantidad Horas Estacionamiento -> double -> che
+cobro por hora -> double -> ch
+
+Salidas:
+cobro final -> double -> cf
+* */
+
+fun calculoCobroEstacionamiento(che: Double, ch: Double): Double {
+    val horasRedondeadas = Math.round(che)
+    val cf = horasRedondeadas * ch
+    return cf
+}
+
+
+/*
+g. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que se
+cobra por m2 y realice una funciòn que le permita saber cuánto cobrar a un cliente.
+
+
+Entradas:
+cantidad Metros Cuadrados -> double -> cm2
+cobro por metro cuadrado -> double -> cxm2
+
+Salidas:
+cobro final -> double -> cf
+* */
+
+fun calculoCobroTrabajoPintura(cm2: Double, cxm2: Double): Double {
+    val cf = cm2 * cxm2
+    return cf
+}
+
+/*
+i. Realice un algoritmo para determinar cuánto pagará finalmente una persona por un artículo equis,
+considerando que tiene un descuento de 5%, y debe pagar 19% de IVA (debe retornar el precio con descuento y
+el precio final)
+
+
+Entradas:
+precioNetoArticulo -> double -> pna
+
+Salidas:
+descuento -> double -> d
+precioFinal -> double -> pf
+* */
+
+fun precioArticulo(pna: Double): Double {
+    val des = pna * 0.05
+    val d = pna - des
+    val pf = d * 0.19
+    return pf
+}
+
+
+/*
+j. Realice una función para determinar la nota final que obtendrá un alumno considerando que realiza tres
+exámenes, de los cuales el primero y el segundo tienen una ponderación de 25%, mientras que el tercero de
+50%.
+
+Entradas:
+nota examen uno -> double -> n1
+nota examen dos -> double -> n2
+nota examen tres -> double -> n3
+
+Salidas:
+
+nota final -> double -> nf
+
+* */
+
+fun calculoNotaFinal(n1: Double, n2: Double, n3: Double): Double {
+    var valorExamenUno = n1 * 0.25
+    var valorExamenDos = n2 * 0.25
+    var valorExamenTres = n3 * 0.50
+    val nf = valorExamenUno + valorExamenDos + valorExamenTres
+    return nf
+}
+
+
+/*
+l. El presidente de la república ha decidido estimular a todos los estudiantes de una universidad mediante la
+asignación de becas mensuales, para esto se tomarán en consideración los siguientes criterios:
+ Para alumnos mayores de 18 años con promedio mayor o igual a 90, la beca será de $2000.00;
+  con promedio mayor o igual a 75, de$1000.00;
+  para los promedios menores de 75 pero mayores o iguales a 60, de $500.00;
+   a los demás se les enviará una carta de invitación incitándolos a que estudien más en el próximo ciclo escolar.
+
+   A los alumnos de 18 años o menores de esta edad, con promedios mayores o iguales a 90, se les dará $3000;
+   con promedios menores a 90 pero mayores o iguales a 80, $2000;
+   para los alumnos con promedios menores a 80 pero mayores o iguales a 60, se les dará $100,
+   y a los alumnos que tengan promedios menores a 6 se les enviará carta de invitación. Realice la función correspondiente.
+
+
+Entradas:
+
+edad estudiante -> int -> ee
+promedio estudiante ->pe
+
+Salidas:
+
+valor beca -> double -> vb
+
+* */
+
+fun calculoValorBeca(ee: Int, pe: Double): Double {
+    return if (ee > 18) valorBecaMayoresEdad(pe)
+    else valorBecaMenorEdad(pe)
+}
+
+fun valorBecaMayoresEdad(pe: Double): Double {
+    if (pe >= 90) return 2000.00
+    else if (pe >= 75) return 1000.00
+    else if (pe < 75 && pe >= 60) return 500.00
+    return 0.0
+}
+
+fun valorBecaMenorEdad(pe: Double): Double {
+    if (pe >= 90) return 3000.00
+    else if (pe < 90 && pe >= 80) return 2000.00
+    else if (pe < 80 && pe >= 60) return 100.00
+    return 0.0
+}
+
+/*
+k. Se tiene el nombre y la edad de tres personas. Se desea saber el nombre y la edad de la persona de menor
+edad.
+
+Entradas:
+persona uno -> double -> p1
+persona dos -> double -> p2
+persona tres -> double -> p3
+
+Salidas:
+
+persona menor -> double -> pm
+
+* */
+
+class Persona {
+    var nombre: String
+    var edad: Int
+
+    constructor(nombre: String, edad: Int) {
+        this.nombre = nombre
+        this.edad = edad
+    }
+}
+
+
+fun calculoPersonaMenor(p1: Persona, p2: Persona, p3: Persona): Pair<String, Int> {
+
+    val listadoPersonas: List<Persona> = listOf(p1, p2, p3)
+
+    val menorEntreP1P2 = Math.min(p1.edad, p2.edad)
+    val menor = Math.min(menorEntreP1P2, p3.edad)
+
+    listadoPersonas.forEach {
+        if (it.edad == menor) {
+            return Pair(it.nombre, it.edad)
+        }
+    }
+    return Pair("persona no existe", 0)
+}
+
+
+/*
+o. Se sabe que una batería de celular contamina 175 litros de agua. ¿Cuántos litros de agua serán contaminados
+por la población de una ciudad en un año si cada persona utiliza 2 baterías al semestre?
+
+Entradas:
+cantidad personas en la ciudad -> int -> psn
+
+Salidas:
+
+litros de agua contaminados -> int -> lac
+
+* */
+
+fun aguaPotableContaminada(psn: Int): Int {
+    val bateriasXSemestre = psn * 2
+    val bateriasAnio = bateriasXSemestre * 2
+    val lac = bateriasAnio * 175
+    return lac
+}
+
+
+/*
+p. Mi proveedor de Internet me asegura que mi conexión es de 4 megabytes por segundo. Escriba una función
+que permita determinar cuántos minutos y segundos debo esperar para descargar la última película de La
+Mujer Maravilla.
+
+Entradas:
+
+peso pelicula en gigabytes -> Double -> ppg
+
+Salidas:
+
+tiempo espera -> int -> te
+
+* */
+
+fun calculoDescargaPelicula(ppg: Double): Double {
+
+    val unGbAMb = 1024
+    //funcion -> ((ppg*UnGbAMb)/4)/(60)
+    val te = ((ppg * unGbAMb) / 4) / 60
+    return te
+}
+
+
+/*
+El valor presente de un cierto capital de dinero a una tasa de interés de durante años se encuentra a partir
+de la fórmula:
+
+VP= c/Math.pow(1+t,n)
+
+Escriba una función que determine el valor presente a partir de los datos de entrada dados
+
+Entradas:
+
+capital -> Double -> c
+tasa interes -> Double ->t
+anios -> Double -> n
+
+Salidas:
+
+VP -> int -> vp
+
+* */
+
+fun calculoValoPresente(c: Double, t: Double, n: Double): Double {
+    val vp = c / (Math.pow(1 + t, n))
+    return vp
+}
 
 
